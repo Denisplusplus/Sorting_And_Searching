d9e01993acf65c1138081777332bd6bba272b45d
Denisplusplus <whinslow@mail.ru>
2017-06-16 19:59:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #define LENGHT 20
+#define SWAP_KEYS(A, B) {int t = A; A = B; B = t;}
+
+void swap_data(char *str1, char *str2)
+{
+  char *temp = (char *)malloc((strlen(str1) + 1) * sizeof(char));
+  strcpy(temp, str1);
+  strcpy(str1, str2);
+  strcpy(str2, temp);
+  free(temp);
+}  
+
 typedef enum {S1, S2} State;
 
 struct str {
@@ -29,11 +41,13 @@ int str_quantity(char* filename)
 }
 
 void table_print(Str *table, int str_amount)
-{
+{	
+	printf("\n");
 	for (int i=0; i<str_amount; i++) {
     	printf("%d ",table[i].key);
     	printf("%s\n", table[i].data);
-   }
+   	}
+   	printf("\n");
 }
 
 void table_data_search(Str *table, int str_amount)
@@ -52,6 +66,24 @@ void table_data_search(Str *table, int str_amount)
 	if (flag == 0) {
 		printf("No matches for key %d found\n", key_to_search);
 	}
+}
+
+void table_linear_sort(Str *table, int str_amount)
+{
+	for(int i = 0; i < (str_amount -1); i++) {
+		for (int j=(i + 1); j < str_amount; j++) {
+			if (table[j].key < table[i].key) {
+				SWAP_KEYS(table[j].key, table[i].key);
+				swap_data(table[j].data, table[i].data);
+			}
+		}
+	}
+
+	for (int i=0; i<str_amount; i++) {
+    	printf("%d ",table[i].key);
+    	printf("%s\n", table[i].data);
+    }
+
 }
 
 Str* table_create(char* filename, Str *table, int str_amount)
@@ -120,8 +152,11 @@ int main (void) {
 	int str_amount=str_quantity(filename);
 	Str *table=(Str *)malloc(sizeof(Str)*str_amount);
 	table = table_create(&filename[0], table, str_amount);
-	table_data_search(table, str_amount);
-	//table_print(table, str_amount);
+	//table_data_search(table, str_amount);
+	table_print(table, str_amount);
+	Str *table_new = table;
+	table_linear_sort(table_new, str_amount);
+
 	
 	return 0;
 }
